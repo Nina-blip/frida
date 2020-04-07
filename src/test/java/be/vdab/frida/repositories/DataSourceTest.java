2e787dfe4af2bb3befd5ce1d3f7f1ee969d11dc9
Nina <nina.vanhoof@vdabcampus.be>
2020-04-07 09:27:02 +0200

    Oefening datasource

---
+++ src/test/java/be/vdab/frida/repositories/DataSourceTest.java
@@ -0,0 +1,25 @@
+package be.vdab.frida.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@JdbcTest
+class DataSourceTest {
+    private final DataSource dataSource;
+
+    @Autowired
+    public DataSourceTest(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Test
+    void getConnection() throws SQLException{
+        try(Connection connection=dataSource.getConnection()) {
+        }
+    }
+}
